96c19d8832c9c7a0fe8f8ebb8d0837d2de6f9d2a
psibean <psionatix@gmail.com>
2021-09-15 19:37:15 +0930

    Add Category

---
+++ src/struct/Category.ts
@@ -0,0 +1,29 @@
+import { Collection } from "discord.js";
+import BaseModule from "./BaseModule";
+
+export default class Category extends Collection<string, BaseModule> {
+  public id: string;
+  
+  public constructor(id: string, iterables: BaseModule[] = []) {
+    super(iterables.map(iterable => [iterable.id, iterable]));
+    this.id = id;
+  }
+
+  public reloaderAll(): Category {
+    for (const module of Array.from(this.values())) {
+      if (module.filepath) module.reload();
+    }
+    return this;
+  }
+
+  public removeAll(): Category {
+    for (const module of Array.from(this.values())) {
+      if (module.filepath) module.remove();
+    }
+    return this;
+  }
+
+  public toString(): string {
+    return this.id;
+  }
+}
