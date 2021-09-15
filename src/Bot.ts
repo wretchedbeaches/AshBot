e5235da2355bf8b79ee52885af8fc9d5af1ee6f7
psibean <psionatix@gmail.com>
2021-09-15 19:35:32 +0930

    Revert the dotenv change

--- src/Bot.ts
+++ src/Bot.ts
@@ -1,8 +1,11 @@
 import BotClient from './client/BotClient';
+import * as dotenv from 'dotenv';
+import { join } from 'path';
+dotenv.config({ path: join(__dirname, 'env.env') });
 
 const client: BotClient = new BotClient({
   token: process.env.TOKEN,
-  owners: process.env.OWNERS,
+  owners: process.env.OWNERS.split(","),
 });
 client.start();
 
