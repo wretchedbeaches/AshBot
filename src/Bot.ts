8f4b939ec3af929df02b856bf4c8a7f03e9221af
psibean <psionatix@gmail.com>
2021-09-21 23:22:20 +0930

    Update client with client_id and initial intents

--- src/Bot.ts
+++ src/Bot.ts
@@ -1,11 +1,15 @@
 import BotClient from './client/BotClient';
 import * as dotenv from 'dotenv';
 import { join } from 'path';
-dotenv.config({ path: join(__dirname, 'env.env') });
+import { Intents } from 'discord.js';
+dotenv.config({ path: join(__dirname, '..', '.env') });
 
 const client: BotClient = new BotClient({
   token: process.env.TOKEN,
+  clientId: process.env.CLIENT_ID,
   owners: process.env.OWNERS.split(","),
+}, {
+  intents: [Intents.FLAGS.GUILDS]
 });
 client.start();
 
