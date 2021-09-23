7e5108726635056aab4a8c9caf4346f34498d616
psibean <psionatix@gmail.com>
2021-09-23 23:27:06 +0930

    Fix Public Commands

--- src/commands/Public Commands/8BallCommand.ts
+++ src/commands/Public Commands/8BallCommand.ts
@@ -1,43 +1,45 @@
-import { CommandInteraction, Interaction } from "discord.js";
-import Command from "../../struct/commands/Command";
+import { CommandInteraction } from 'discord.js';
+import Command from '../../struct/commands/Command';
 
 const responses = [
-    "It is certain",
-    "It is decidedly so",
-    "Without a doubt",
-    "Yes, definitely",
-    "You may rely on it",
-    "As I see it, yes",
-    "Most likely",
-    "Outlook good",
-    "Yes",
-    "Signs point to yes",
-    "Reply hazy try again",
-    "Ask again later",
-    "Better not tell you now",
-    "Cannot predict now",
-    "Concentrate and ask again",
-    "Don't count on it",
-    "My reply is no",
-    "My sources say no",
-    "Outlook not so good",
-    "Very doubtful",
+	'It is certain',
+	'It is decidedly so',
+	'Without a doubt',
+	'Yes, definitely',
+	'You may rely on it',
+	'As I see it, yes',
+	'Most likely',
+	'Outlook good',
+	'Yes',
+	'Signs point to yes',
+	'Reply hazy try again',
+	'Ask again later',
+	'Better not tell you now',
+	'Cannot predict now',
+	'Concentrate and ask again',
+	"Don't count on it",
+	'My reply is no',
+	'My sources say no',
+	'Outlook not so good',
+	'Very doubtful',
 ];
 
 export default class EightBall extends Command {
-    public constructor() {
-        super('8ball', {
-            helpDescription: {
-                content: "Let the bot decide your future",
-                usage: "Fortune-Telling Game",
-                examples: ["8ball will i get a gf"],
-            },
-            description: 'Let the bot decide your future...',
-        });
-        this.data.addStringOption(option => option.setName('question').setDescription('What would you like to ask the 8ball?'));
-    }
+	public constructor() {
+		super('8ball', {
+			helpDescription: {
+				content: 'Let the bot decide your future',
+				usage: 'Fortune-Telling Game',
+				examples: ['8ball will i get a gf'],
+			},
+			description: 'Let the bot decide your future...',
+		});
+		this.data.addStringOption((option) =>
+			option.setName('question').setDescription('What would you like to ask the 8ball?'),
+		);
+	}
 
-    public execute(interaction: CommandInteraction) {
-        return interaction.editReply({ content: responses[Math.floor(Math.random() * responses.length - 1)] });
-    }
+	public execute(interaction: CommandInteraction) {
+		return interaction.editReply({ content: responses[Math.floor(Math.random() * responses.length - 1)] });
+	}
 }
--- src/commands/Public Commands/PingCommand.ts
+++ src/commands/Public Commands/PingCommand.ts
@@ -1,21 +1,21 @@
-import { CommandInteraction } from "discord.js";
-import Command from "../../struct/commands/Command";
+import { CommandInteraction } from 'discord.js';
+import Command from '../../struct/commands/Command';
 
 export default class PingCommand extends Command {
-  public constructor() {
-    super('ping', {
-      category: 'Public Commands',
-      helpDescription: {
-        content: 'Check the latency of the ping to the Discord API',
-        usage: 'ping',
-        examples: ['ping'],
-      },
-      description: 'Check the latency of the ping to the Discord APU',
-      ratelimit: 3,
-    });
-  }
+	public constructor() {
+		super('ping', {
+			category: 'Public Commands',
+			helpDescription: {
+				content: 'Check the latency of the ping to the Discord API',
+				usage: 'ping',
+				examples: ['ping'],
+			},
+			description: 'Check the latency of the ping to the Discord APU',
+			ratelimit: 3,
+		});
+	}
 
-  public execute(interaction: CommandInteraction) {
-    return interaction.editReply({ content: `Pong! \`${this.client.ws.ping}ms\`` });
-  }
+	public execute(interaction: CommandInteraction) {
+		return interaction.editReply({ content: `Pong! \`${this.client.ws.ping}ms\`` });
+	}
 }
