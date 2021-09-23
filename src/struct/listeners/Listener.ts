148c6e8fa612cdfd9a839fd5b25f1343d5c3ce78
psibean <psionatix@gmail.com>
2021-09-23 22:55:04 +0930

    Fix listener and listener handler

--- src/struct/listeners/Listener.ts
+++ src/struct/listeners/Listener.ts
@@ -1,29 +1,29 @@
-import { EventEmitter } from "events";
-import BaseModule, { BaseModuleOptions } from "../BaseModule";
-import { ErrorMessages } from "../Util";
-import ListenerHandler from "./ListenerHandler";
+import { EventEmitter } from 'events';
+import BaseModule, { BaseModuleOptions } from '../BaseModule';
+import { ErrorMessages } from '../Util';
+import ListenerHandler from './ListenerHandler';
 
 export interface ListenerOptions extends BaseModuleOptions {
-    emitter: string | EventEmitter;
-    event: string;
-    type?: string;
+	emitter: string | EventEmitter;
+	event: string;
+	type?: string;
 }
 
 export default class Listener extends BaseModule {
-    public emitter: string | EventEmitter;
-    public event: string;
-    public type: string;
-    public handler: ListenerHandler;
+	public emitter: string | EventEmitter;
+	public event: string;
+	public type: string;
+	public handler: ListenerHandler;
 
-    constructor(id, { emitter, event, type = 'on', ...rest }: ListenerOptions) {
-        super(id, rest);
-        this.emitter = emitter;
-        this.event = event;
-        this.type = type;
-    }
+	public constructor(id, { emitter, event, type = 'on', ...rest }: ListenerOptions) {
+		super(id, rest);
+		this.emitter = emitter;
+		this.event = event;
+		this.type = type;
+	}
 
-    public execute(...args: any[]): any {
-        throw new Error(ErrorMessages.NOT_IMPLEMENTED(this.constructor.name, 'execute'));
-    }
-};
-
+	// eslint-disable-next-line @typescript-eslint/no-unused-vars
+	public execute(...args: any[]): any {
+		throw new Error(ErrorMessages.NOT_IMPLEMENTED(this.constructor.name, 'execute'));
+	}
+}
--- src/struct/listeners/ListenerHandler.ts
+++ src/struct/listeners/ListenerHandler.ts
@@ -1,62 +1,64 @@
-import { Collection } from "discord.js";
-import { EventEmitter } from "events";
-import BaseHandler, { BaseHandlerOptions } from "../BaseHandler";
-import { ErrorMessages } from "../Util";
-import Listener from "./Listener";
+import { Collection } from 'discord.js';
+import { EventEmitter } from 'events';
+import BaseHandler, { BaseHandlerOptions } from '../BaseHandler';
+import { ErrorMessages } from '../Util';
+import Listener from './Listener';
 
 export default class ListenerHandler extends BaseHandler {
-    public emitters: Collection<string, EventEmitter>;
-    public modules: Collection<string, Listener>;
-    public classToHandle: new (...args: any[]) => Listener;
+	public emitters: Collection<string, EventEmitter>;
+	public modules: Collection<string, Listener>;
+	public classToHandle: new (...args: any[]) => Listener;
 
-    constructor(client, options: BaseHandlerOptions) {
-        super(client, options);
-        this.classToHandle = Listener;
-        this.emitters = new Collection();
-        this.emitters.set('client', this.client);
-    }
+	public constructor(client, options: BaseHandlerOptions) {
+		super(client, options);
+		this.classToHandle = Listener;
+		this.emitters = new Collection();
+		this.emitters.set('client', this.client);
+	}
 
-    public register(listener: Listener, filepath: string): Listener {
-        super.register(listener, filepath);
-        listener.execute = listener.execute.bind(listener);
-        this.addToEmitter(listener.id);
-        return listener;
-    }
+	public register(listener: Listener, filepath: string): Listener {
+		super.register(listener, filepath);
+		listener.execute = listener.execute.bind(listener);
+		this.addToEmitter(listener.id);
+		return listener;
+	}
 
-    public deregister(listener: Listener): void {
-        this.removeFromEmitter(listener.id);
-        super.deregister(listener);
-    }
+	public deregister(listener: Listener): void {
+		this.removeFromEmitter(listener.id);
+		super.deregister(listener);
+	}
 
-    public addToEmitter(id: string): Listener {
-        const listener = this.modules.get(id.toString());
-        if (!listener) throw new Error(ErrorMessages.MODULE_NOT_FOUND(this.classToHandle.name, id));
+	public addToEmitter(id: string): Listener {
+		const listener = this.modules.get(id);
+		if (!listener) throw new Error(ErrorMessages.MODULE_NOT_FOUND(this.classToHandle.name, id));
 
-        const emitter = listener.emitter instanceof EventEmitter ? listener.emitter : this.emitters.get(listener.emitter);
+		const emitter = listener.emitter instanceof EventEmitter ? listener.emitter : this.emitters.get(listener.emitter);
+		if (!emitter) return listener;
 
-        if (listener.type === 'once') {
-            emitter.once(listener.event, listener.execute);
-            return listener;
-        }
+		if (listener.type === 'once') {
+			emitter.once(listener.event, listener.execute);
+			return listener;
+		}
 
-        emitter.on(listener.event, listener.execute);
-        return listener;
-    }
+		emitter.on(listener.event, listener.execute);
+		return listener;
+	}
 
-    public removeFromEmitter(id: string): Listener {
-        const listener = this.modules.get(id.toString());
-        if (!listener) throw new Error(ErrorMessages.MODULE_NOT_FOUND(this.classToHandle.name, id));
+	public removeFromEmitter(id: string): Listener {
+		const listener = this.modules.get(id);
+		if (!listener) throw new Error(ErrorMessages.MODULE_NOT_FOUND(this.classToHandle.name, id));
+		const emitter = listener.emitter instanceof EventEmitter ? listener.emitter : this.emitters.get(listener.emitter);
+		if (!emitter) return listener;
 
-        const emitter = listener.emitter instanceof EventEmitter ? listener.emitter : this.emitters.get(listener.emitter);
-        emitter.removeListener(listener.event, listener.execute);
-        return listener;
-    }
+		emitter.removeListener(listener.event, listener.execute);
+		return listener;
+	}
 
-    public setEmitters(emitters: { [key: string ]: EventEmitter }) {
-        for (const [key, value] of Object.entries(emitters)) {
-            this.emitters.set(key, value);
-        }
+	public setEmitters(emitters: { [key: string]: EventEmitter }) {
+		for (const [key, value] of Object.entries(emitters)) {
+			this.emitters.set(key, value);
+		}
 
-        return this;
-    }
-};
+		return this;
+	}
+}
