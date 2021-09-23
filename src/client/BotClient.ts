1b809e038b27e5e4134fa242af79c07c2c062a0d
psibean <psionatix@gmail.com>
2021-09-24 01:29:31 +0930

    Lint BotCLient

--- src/client/BotClient.ts
+++ src/client/BotClient.ts
@@ -1,75 +1,248 @@
-import { Client, ClientOptions, User } from "discord.js";
+import { Client, ClientOptions, Collection, MessageEmbed, User } from 'discord.js';
 import { REST } from '@discordjs/rest';
-import CommandHandler from "../struct/commands/CommandHandler";
-import InhibitorHandler from "../struct/inhibitors/InhibitorHandler";
-import ListenerHandler from "../struct/listeners/ListenerHandler";
-import { join } from "path";
+import CommandHandler from '../struct/commands/CommandHandler';
+import InhibitorHandler from '../struct/inhibitors/InhibitorHandler';
+import ListenerHandler from '../struct/listeners/ListenerHandler';
+import SequelizeProvider from '../util/SequelizeProvider';
+import { Sequelize } from 'sequelize';
+import { initModels as initManualModels } from '../manualdbModels/init-models';
+import { initModels } from '../rdmdbModels/init-models';
+import puppeteer from 'puppeteer';
+// import { device } from '../rdmdbModels/device';
+import { guild } from '../rdmdbModels/guild';
+import { join } from 'path';
+// import { WebhookFilter } from '../models/WebhookFilters';
+import express from 'express';
+import hookRouter from '../routes/hook';
+import cors from 'cors';
 
 interface BotConfig {
-  token?: string;
-  clientId?: string;
-  owners?: string[];
+	token: string;
+	clientId: string;
+	owners: string[];
 }
 
 export interface BaseClientAttributes extends Client {
-  config: BotConfig;
-  owners: string[];
-  restApi: REST;
-  commandHandler: CommandHandler;
-  listenerHandler: ListenerHandler;
-  inhibitorHandler: InhibitorHandler;
-  isOwner(user: User): boolean;
+	config: BotConfig;
+	owners: string[];
+	restApi: REST;
+	commandHandler: CommandHandler;
+	listenerHandler: ListenerHandler;
+	inhibitorHandler: InhibitorHandler;
+	isOwner(user: User): boolean;
 }
 
 export default class BaseClient extends Client implements BaseClientAttributes {
-  public owners: string[];
-  public config: BotConfig;
-  public restApi: REST;
-  public commandHandler: CommandHandler;
-  public listenerHandler: ListenerHandler;
-  public inhibitorHandler: InhibitorHandler;
+	public owners: string[];
+	public config: BotConfig;
+	public restApi: REST;
+	public commandHandler: CommandHandler;
+	public listenerHandler: ListenerHandler;
+	public inhibitorHandler: InhibitorHandler;
+	public nestMigrationDate: Date;
+	public settings: SequelizeProvider;
+	public embedQueue: Collection<string, MessageEmbed[]>;
+	public intervals: Collection<string, NodeJS.Timeout>;
 
-  public constructor(config: BotConfig, options: ClientOptions) {
-    super(options);
-    this.config = config;
-    this.owners = config.owners;
-    this.restApi = new REST({ version: '9' }).setToken(config.token);
+	public constructor(config: BotConfig, options: ClientOptions) {
+		super(options);
+		this.config = config;
+		this.owners = config.owners;
+		this.restApi = new REST({ version: '9' }).setToken(config.token);
 
-    this.listenerHandler =  new ListenerHandler(this, {
-      directory: join(__dirname, '..', 'listeners'),
-    });
+		this.listenerHandler = new ListenerHandler(this, {
+			directory: join(__dirname, '..', 'listeners'),
+		});
 
-    this.commandHandler = new CommandHandler(this, {
-      directory: join(__dirname, '..', 'commands/Public Commands'),
-      defaultCooldown: 6e4
-    });
-    this.inhibitorHandler = new InhibitorHandler(this, {
-      directory: join(__dirname, '..', 'inhibitors'),
-    });
-  }
+		this.commandHandler = new CommandHandler(this, {
+			directory: join(__dirname, '..', 'commands/Public Commands'),
+			defaultCooldown: 6e4,
+		});
+		this.inhibitorHandler = new InhibitorHandler(this, {
+			directory: join(__dirname, '..', 'inhibitors'),
+		});
 
-  private async _init(): Promise<void> {
-    this.listenerHandler.setEmitters({
-      commandHandler: this.commandHandler,
-      listenerHandler: this.listenerHandler,
-      process,
-    });
-    this.listenerHandler.loadAll();
-    this.commandHandler.useListenerHandler(this.listenerHandler);
-    this.commandHandler.useInhibitorHandler(this.inhibitorHandler);
-  }
+		this.settings = new SequelizeProvider(guild, {
+			idColumn: 'id',
+			dataColumn: 'settings',
+		});
+	}
 
-  public async start() {
-    await this._init();
-    await this.login(this.config.token);
-    // await this.initGuilds();
-    // await this.initServer();
-  }
+	private async _init(): Promise<void> {
+		this.listenerHandler.setEmitters({
+			commandHandler: this.commandHandler,
+			listenerHandler: this.listenerHandler,
+			process,
+		});
+		await this.listenerHandler.loadAll();
+		this.commandHandler.useListenerHandler(this.listenerHandler);
+		this.commandHandler.useInhibitorHandler(this.inhibitorHandler);
 
-  public isOwner(user) {
-    return this.owners.includes(user.id);
-  }
+		await this.updateNestMigrationDate();
+		// eslint-disable-next-line @typescript-eslint/no-misused-promises
+		setInterval(this.updateNestMigrationDate, parseInt(process.env.NEST_MIGRATION_UPDATE_TIME!, 10));
 
+		await this.commandHandler.loadAll();
+		await this.listenerHandler.loadAll();
+		await this.inhibitorHandler.loadAll();
+		const rdmdb = new Sequelize(process.env.DATABASE!, process.env.DATABASE_USERNAME!, process.env.DATABASE_PASSWORD, {
+			host: process.env.DATABASE_HOST,
+			dialect: 'mysql',
+			logging: false,
+		});
+		const manualdb = new Sequelize(
+			process.env.MANUAL_DATABASE!,
+			process.env.MANUAL_DATABASE_USERNAME!,
+			process.env.MANUAL_DATABASE_PASSWORD,
+			{
+				host: process.env.MANUAL_DATABASE_HOST,
+				dialect: 'mysql',
+				logging: false,
+			},
+		);
+		initModels(rdmdb);
+		initManualModels(manualdb);
+		await rdmdb.sync();
+		await manualdb.sync();
+		console.log('[Bot] Connected to database');
+		await this.settings.init();
+	}
+
+	public async updateNestMigrationDate() {
+		try {
+			const browser = await puppeteer.launch();
+			const page = await browser.newPage();
+			await page.goto('https://p337.info/pokemongo/countdowns/?id=nest-migration');
+			await page.waitForSelector('#local_time3', {
+				visible: true,
+				timeout: 10000,
+			});
+			const nestMigrationDate = await page.evaluate(() => document.getElementById('local_time3')?.innerHTML);
+			this.nestMigrationDate = new Date(nestMigrationDate!);
+			await browser.close();
+		} catch (e) {
+			console.log('[Bot] Error while attempting to grab nest migration date:', e);
+		}
+	}
+
+	public async start() {
+		await this._init();
+		await this.login(this.config.token);
+		// TODO: Figure out what the embed type is / where it's coming from.
+		// Where does the .shiny and .coordinates properties come from?
+		// Where are these initially added into the embed queue.
+		// Seems to come from hook.ts - no object types defined there.
+		// await this.initGuilds();
+		await this.initServer();
+	}
+
+	// private async initGuilds(): Promise<void> {
+	//   for (let [_, { channels }] of this.settings.items) {
+	//     for (let channelId in channels) {
+	//       this.embedQueue.set(channelId, []);
+	//       try {
+	//         this.intervals.set(channelId, setInterval(async () => {
+	//           if (
+	//             this.embedQueue.has(channelId) &&
+	//             this.embedQueue.get(channelId).length > 0
+	//           ) {
+	//             // fetch checks cache first already
+	//             const channel = await this.channels.fetch(channelId);
+	//             if (!channel || !channel.isText()) return;
+	//             if (this.embedQueue.has(channelId)) {
+	//               const embedQueue = this.embedQueue.get(channelId);
+	//               const embed = await embedQueue[0];
+	//               const embedMessage = await channel.send({
+	//                 content: embed.coordinates
+	//                   ? `${embed.coordinates[0].toFixed(
+	//                   5
+	//                 )}, ${embed.coordinates[1].toFixed(5)}`
+	//               : embed.message,
+	//               embeds: [embed.embed],
+	//             });
+	//           if (embed.shiny)
+	//             this.handleShinyReactions(embedMessage, embed.user);
+	//           this.embedQueue[channelId] = this.embedQueue[channelId].slice(
+	//             1,
+	//             this.embedQueue[channelId].length
+	//           );
+	//             }
+	//           }
+	//         }, Number(process.env.QUEUE_INTERVAL)));
+	//       } catch (error) {
+	//         console.log(error);
+	//       }
+	//     }
+	//   }
+	// }
+
+	private initServer() {
+		const PORT = process.env.PORT ?? 8080;
+		const app = express();
+		app.use(express.json({ limit: '500mb' }));
+		app.use(cors());
+		app.use('/', hookRouter);
+
+		app.listen(PORT, () => {
+			console.log(`[Bot] Listening for webhooks on: http://localhost:${PORT}.`);
+		});
+	}
+
+	// public async handleWebhookSet(interaction: CommandInteraction, args: WebhookFilter) {
+	//   const channelId = interaction.channel.id;
+	//   const guildId = interaction.guild.id;
+	//   if (args.rmchannel) {
+	//     const settings = this.settings.get(guildId, 'channels', {});
+	//     delete settings[channelId];
+	//     await this.settings.set(channelId, 'channels', settings);
+	//     if (this.intervals[channelId]) {
+	//       clearInterval(channelId);
+	//       delete this.intervals[channelId];
+	//       delete this.embedQueue[channelId];
+	//       delete this.trains[channelId];
+	//     }
+	//     return interaction.editReply(
+	//       `Successfully removed channel's ${args.type} webhook configuration.`
+	//     );
+	//   } else {
+	//     const settings = this.settings.get(guildId, 'channels', {});
+	//     for (let arg in args)
+	//       if (args[arg] === null || arg === 'rmchannel') delete args[arg];
+	//     settings[channelId] = args;
+	//     await this.settings.set(channelId, 'channels', settings);
+	//     if (this.intervals[channelId])
+	//       clearInterval(this.intervals[channelId]);
+	//     this.embedQueue[channelId] = [];
+	//     this.intervals[channelId] = setInterval(async () => {
+	//       if (
+	//         this.embedQueue[channelId] &&
+	//         this.embedQueue[channelId].length > 0
+	//       ) {
+	//         const embed = this.embedQueue[message.channel.id][0];
+	//         const embedMessage = await channel.send({
+	//           content: embed.coordinates
+	//             ? `${embed.coordinates[0].toFixed(
+	//                 5
+	//               )}, ${embed.coordinates[1].toFixed(5)}`
+	//             : embed.message,
+	//           embeds: embed.embed,
+	//         });
+	//         if (embed.shiny) this.handleShinyReactions(embedMessage, embed.user);
+	//         this.embedQueue[message.channel.id] = this.embedQueue[
+	//           message.channel.id
+	//         ].slice(1, this.embedQueue[message.channel.id].length);
+	//       }
+	//     }, Number(process.env.QUEUE_INTERVAL));
+	//     delete this.trains[message.channel.id];
+	//     return message.util.send(
+	//       `Successfully updated channel's ${args.type} webhook configuration.`
+	//     );
+	//   }
+	// }
+
+	public isOwner(user) {
+		return this.owners.includes(user.id);
+	}
 }
 // import {
 //   CommandHandler,
