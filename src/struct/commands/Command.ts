f3f01ca975682c0ed9568ddc781979fd7053c67a
psibean <psionatix@gmail.com>
2021-09-23 22:39:03 +0930

    Update command and command handler

--- src/struct/commands/Command.ts
+++ src/struct/commands/Command.ts
@@ -1,93 +1,87 @@
-import { SlashCommandBuilder } from "@discordjs/builders";
-import type { APIApplicationCommandOption } from 'discord-api-types/v9';
-import { CommandInteraction, PermissionFlags, ThreadChannelTypes } from "discord.js";
-import BaseModule, { BaseModuleOptions } from "../BaseModule";
-import CommandHandler from "./CommandHandler";
-import { ErrorMessages } from "../Util";
+import { SlashCommandBuilder } from '@discordjs/builders';
+import { CommandInteraction, PermissionFlags, ThreadChannelTypes } from 'discord.js';
+import BaseModule, { BaseModuleOptions } from '../BaseModule';
+import CommandHandler from './CommandHandler';
+import { ErrorMessages } from '../Util';
 
-export type CommandScope = "global" | "guild";
-export type CommandHelpDescription = {
-    content?: string;
-    usage?: string,
-    examples?: string[]
+export type CommandScope = 'global' | 'guild';
+export interface CommandHelpDescription {
+	content: string;
+	usage?: string;
+	examples?: string[];
 }
 
 export interface CommandOptions extends BaseModuleOptions {
-    channels?: CommandChannelType[];
-    ownerOnly?: boolean;
-    cooldown?: number;
-    ignoreCooldown?: Array<string> | ((interaction: CommandInteraction, command: Command) => boolean);
-    ratelimit?: number;
-    description?: string;
-    helpDescription?: CommandHelpDescription;
-    clientPermissions?: Set<PermissionFlags>;
-    userPermissions?: Set<PermissionFlags>;
-    shouldDefer?: boolean;
-    scope?: CommandScope;
+	channels?: CommandChannelType[];
+	ownerOnly?: boolean;
+	cooldown?: number;
+	ignoreCooldown?: Array<string> | ((interaction: CommandInteraction, command: Command) => boolean);
+	ratelimit?: number;
+	description?: string;
+	helpDescription: CommandHelpDescription;
+	clientPermissions?: Set<PermissionFlags>;
+	userPermissions?: Set<PermissionFlags>;
+	shouldDefer?: boolean;
+	scope?: CommandScope;
 }
 
-export type CommandDataType = {
-    name: string;
-    description: string;
-    options: APIApplicationCommandOption[];
-    default_permission: boolean | undefined;
-}
-
-export type CommandChannelType = "DM" | "GUILD_TEXT" | "GUILD_NEWS" | ThreadChannelTypes;
+export type CommandChannelType = 'DM' | 'GUILD_TEXT' | 'GUILD_NEWS' | ThreadChannelTypes;
 
 export default class Command extends BaseModule {
-    public channels: Set<CommandChannelType>;
-    public ownerOnly: boolean;
-    public cooldown: number;
-    public ignoreCooldown: Array<string> | ((interaction: CommandInteraction, command: Command) => boolean);
-    public ratelimit: number;
-    public description: string;
-    public helpDescription: CommandHelpDescription;
-    public clientPermissions: Set<PermissionFlags>;
-    public userPermissions: Set<PermissionFlags>;
-    public shouldDefer: boolean;
-    public scope: CommandScope;
-    public data: SlashCommandBuilder;
-    public handler: CommandHandler;
+	public channels: Set<CommandChannelType>;
+	public ownerOnly: boolean;
+	public cooldown: number;
+	public ignoreCooldown?: Array<string> | ((interaction: CommandInteraction, command: Command) => boolean);
+	public ratelimit: number;
+	public description: string;
+	public helpDescription: CommandHelpDescription;
+	public clientPermissions: Set<PermissionFlags>;
+	public userPermissions: Set<PermissionFlags>;
+	public shouldDefer: boolean;
+	public scope: CommandScope;
+	public data: SlashCommandBuilder;
+	public handler: CommandHandler;
 
-    public constructor(id: string, {
-        channels = [],
-        ownerOnly = false,
-        cooldown = null,
-        ignoreCooldown = null,
-        ratelimit = 1,
-        description = '',
-        helpDescription = {},
-        clientPermissions = new Set(),
-        userPermissions = new Set(),
-        shouldDefer = true,
-        scope = "global",
-        ...rest
-    }: CommandOptions) {
-        super(id, rest);
-        this.channels = new Set();
-        if (Array.isArray(channels))
-            for (const channel of channels)
-                this.channels.add(channel);
-        this.ownerOnly = ownerOnly;
-        this.cooldown = cooldown;
-        this.ignoreCooldown = ignoreCooldown;
-        this.ratelimit = ratelimit;
-        this.helpDescription = helpDescription;
-        this.description = description;
-        this.clientPermissions = clientPermissions;
-        this.userPermissions = userPermissions;
-        this.ignoreCooldown = typeof ignoreCooldown === 'function' ? ignoreCooldown.bind(this) : ignoreCooldown;
-        this.shouldDefer = shouldDefer;
-        this.scope = scope;
-        this.data = new SlashCommandBuilder().setName(id).setDescription(this.description);
-    }
+	public constructor(
+		id: string,
+		{
+			channels = [],
+			ownerOnly = false,
+			cooldown = 0,
+			ignoreCooldown,
+			ratelimit = 1,
+			description = '',
+			helpDescription,
+			clientPermissions = new Set(),
+			userPermissions = new Set(),
+			shouldDefer = true,
+			scope = 'global',
+			...rest
+		}: CommandOptions,
+	) {
+		super(id, rest);
+		this.channels = new Set();
+		if (Array.isArray(channels)) for (const channel of channels) this.channels.add(channel);
+		this.ownerOnly = ownerOnly;
+		this.cooldown = cooldown;
+		this.ignoreCooldown = typeof ignoreCooldown === 'function' ? ignoreCooldown.bind(this) : ignoreCooldown;
+		this.ratelimit = ratelimit;
+		this.helpDescription = helpDescription;
+		this.description = description;
+		this.clientPermissions = clientPermissions;
+		this.userPermissions = userPermissions;
+		this.shouldDefer = shouldDefer;
+		this.scope = scope;
+		this.data = new SlashCommandBuilder().setName(id).setDescription(this.description);
+	}
 
-    public shouldExecute(interaction: CommandInteraction): Promise<boolean> | boolean {
-        return true;
-    }
+	// eslint-disable-next-line @typescript-eslint/no-unused-vars
+	public shouldExecute(_: CommandInteraction): Promise<boolean> | boolean {
+		return true;
+	}
 
-    public execute(interaction: CommandInteraction): Promise<any> | any {
-        throw new Error(ErrorMessages.NOT_IMPLEMENTED(this.constructor.name, "execute"));
-    }
-};
+	// eslint-disable-next-line @typescript-eslint/no-unused-vars
+	public execute(_: CommandInteraction): Promise<any> | any {
+		throw new Error(ErrorMessages.NOT_IMPLEMENTED(this.constructor.name, 'execute'));
+	}
+}
--- src/struct/commands/CommandHandler.ts
+++ src/struct/commands/CommandHandler.ts
@@ -1,266 +1,280 @@
-import { APIApplicationCommand, Routes } from "discord-api-types/v9";
-import { Collection, CommandInteraction, Interaction } from "discord.js";
-import BaseClient from "../../client/BotClient";
-import BaseHandler, { BaseHandlerOptions } from "../BaseHandler";
-import InhibitorHandler from "../inhibitors/InhibitorHandler";
-import ListenerHandler from "../listeners/ListenerHandler";
-import { CommandHandlerEvents } from "../Util";
-import Command, { CommandDataType } from "./Command";
+import { APIApplicationCommand, Routes } from 'discord-api-types/v9';
+import { Collection, CommandInteraction, Interaction } from 'discord.js';
+import BaseClient from '../../client/BotClient';
+import BaseHandler, { BaseHandlerOptions } from '../BaseHandler';
+import InhibitorHandler from '../inhibitors/InhibitorHandler';
+import ListenerHandler from '../listeners/ListenerHandler';
+import { CommandHandlerEvents } from '../Util';
+import Command from './Command';
 
 export interface CommandHandlerOptions extends BaseHandlerOptions {
-    blockClient?: boolean;
-    blockBots?: boolean;
-    defaultCooldown?: number;
-    ignoreCooldown?: string[];
+	blockClient?: boolean;
+	blockBots?: boolean;
+	defaultCooldown?: number;
+	ignoreCooldown?: string[];
 }
 
-export type CooldownData = {
-    timer?: NodeJS.Timeout;
-    end?: number;
-    uses?: number;
+export interface CooldownData {
+	timer: NodeJS.Timeout;
+	end: number;
+	uses: number;
 }
 
-export type CommandBlockedData = {
-    interaction: Interaction;
-    command: Command;
-    reason: string;
+export interface CommandBlockedData {
+	interaction: Interaction;
+	command: Command;
+	reason: string;
 }
 
 export default class CommandHandler extends BaseHandler {
-    public blockClient: boolean;
-    public blockBots: boolean;
-    public defaultCooldown: number;
-    public ignoreCooldown: string[];
-    public cooldowns: Collection<string, CooldownData>;
-    public inhibitorHandler: InhibitorHandler;
-    public listenerHandler: ListenerHandler;
-    public modules: Collection<string, Command>;
-    public classToHandle: new (...args: any[]) => Command;
+	public blockClient: boolean;
+	public blockBots: boolean;
+	public defaultCooldown: number;
+	public ignoreCooldown: string[];
+	public cooldowns: Collection<string, Collection<string, CooldownData>>;
+	public inhibitorHandler?: InhibitorHandler;
+	public listenerHandler: ListenerHandler;
+	public modules: Collection<string, Command>;
+	public classToHandle: new (...args: any[]) => Command;
 
-    public constructor(client: BaseClient, {
-        directory,
-        automateCategories,
-        blockClient = true,
-        blockBots = true,
-        defaultCooldown = 0,
-        ignoreCooldown = client.owners,
-    }: CommandHandlerOptions) {
-        super(client, {
-            directory,
-            automateCategories,
-        });
-        this.classToHandle = Command;
-        this.blockClient = blockClient;
-        this.blockBots = blockBots;
-        this.cooldowns = new Collection();
-        this.defaultCooldown = defaultCooldown;
-        this.ignoreCooldown = ignoreCooldown;
-        this.inhibitorHandler = null;
-        this.setup();
-    }
+	public constructor(
+		client: BaseClient,
+		{
+			directory,
+			automateCategories,
+			blockClient = true,
+			blockBots = true,
+			defaultCooldown = 0,
+			ignoreCooldown = client.owners,
+		}: CommandHandlerOptions,
+	) {
+		super(client, {
+			directory,
+			automateCategories,
+		});
+		this.classToHandle = Command;
+		this.blockClient = blockClient;
+		this.blockBots = blockBots;
+		this.cooldowns = new Collection();
+		this.defaultCooldown = defaultCooldown;
+		this.ignoreCooldown = ignoreCooldown;
+		this.inhibitorHandler = undefined;
+		this.setup();
+	}
 
-    public setup(): void {
-        this.client.once('ready', async () => {
-            this.client.on('interactionCreate', (interaction: Interaction) => {
-                if (interaction.isCommand())
-                    this.handle(interaction);
-            });
-            await this.loadAll();
-        });
-    }
+	public setup(): void {
+		this.client.once('ready', async () => {
+			this.client.on('interactionCreate', (interaction: Interaction) => {
+				// eslint-disable-next-line @typescript-eslint/no-floating-promises
+				if (interaction.isCommand()) this.handle(interaction);
+			});
+			await this.loadAll();
+		});
+	}
 
-    public getGlobalCommands(): Promise<APIApplicationCommand[]> {
-       return this.client.restApi.get(Routes.applicationCommands(this.client.config.clientId) as unknown as `/${string}`) as Promise<APIApplicationCommand[]>;
-    }
+	public getGlobalCommands(): Promise<APIApplicationCommand[]> {
+		return this.client.restApi.get(
+			Routes.applicationCommands(this.client.config.clientId) as unknown as `/${string}`,
+		) as Promise<APIApplicationCommand[]>;
+	}
 
-    public getGuildCommands(guildId: string): Promise<APIApplicationCommand[]> {
-        return this.client.restApi.get(Routes.applicationGuildCommands(this.client.config.clientId, guildId) as unknown as `/${string}`) as Promise<APIApplicationCommand[]>;
-    }
+	public getGuildCommands(guildId: string): Promise<APIApplicationCommand[]> {
+		return this.client.restApi.get(
+			Routes.applicationGuildCommands(this.client.config.clientId, guildId) as unknown as `/${string}`,
+		) as Promise<APIApplicationCommand[]>;
+	}
 
-    public registerCommandGlobally(command: Command): Promise<APIApplicationCommand> {
-        return this.client.restApi.post(Routes.applicationCommands(this.client.config.clientId) as unknown as `/${string}`, { body: command.data.toJSON() }) as Promise<APIApplicationCommand>;
-    }
+	public registerCommandGlobally(command: Command): Promise<APIApplicationCommand> {
+		return this.client.restApi.post(
+			Routes.applicationCommands(this.client.config.clientId) as unknown as `/${string}`,
+			{ body: command.data.toJSON() },
+		) as Promise<APIApplicationCommand>;
+	}
 
-    public async registerCommandsGlobally(commands: Command[]) {
-        return this._registerCommandsGlobally(this.commandsToData(commands));
-    }
+	public async registerCommandsGlobally(commands: Command[]) {
+		return this._registerCommandsGlobally(this.commandsToData(commands));
+	}
 
-    public _registerCommandsGlobally(commands: any[]) {
-        return this.client.restApi.put(Routes.applicationCommands(this.client.config.clientId) as unknown as `/${string}`, { body: commands})
-    }
+	public _registerCommandsGlobally(commands: any[]) {
+		return this.client.restApi.put(Routes.applicationCommands(this.client.config.clientId) as unknown as `/${string}`, {
+			body: commands,
+		});
+	}
 
-    public registerCommandsForGuild(commands: Command[], guildId: string) {
-        return this._registerCommandsForGuild(this.commandsToData(commands), guildId);
-    }
+	public registerCommandsForGuild(commands: Command[], guildId: string) {
+		return this._registerCommandsForGuild(this.commandsToData(commands), guildId);
+	}
 
-    public _registerCommandsForGuild(commands: any[], guildId: string) {
-        return this.client.restApi.put(Routes.applicationGuildCommands(this.client.config.clientId, guildId) as unknown as `/${string}`, { body: commands });
-    }
+	public _registerCommandsForGuild(commands: any[], guildId: string) {
+		return this.client.restApi.put(
+			Routes.applicationGuildCommands(this.client.config.clientId, guildId) as unknown as `/${string}`,
+			{ body: commands },
+		);
+	}
 
-    public async loadAll(directory: string = this.directory): Promise<CommandHandler> {
-        super.loadAll(directory);
-        // TODO: WIll need to update the global registration logic
-        // Filter all global commands with the names of the registered commands.
-        // For any that aren't registered, register them each individually instead
-        const registeredCommands = await this.getGlobalCommands();
-        const globalCommands = this.modules.filter(command => command.scope === 'global');
-        if (registeredCommands.length === globalCommands.size) return this;
-        // TODO: guild restricted logic
-        // Guild restricted commands should be registered against a list of guilds in the DB to load them for.
-        // const guildCommands = this.modules.filter(command => command.scope !== 'global');
-        const promises = [];
-        promises.push(this.registerCommandsGlobally(Array.from(globalCommands.values())));
-        await Promise.all(promises);
-        return this;
-    }
+	public async loadAll(directory: string = this.directory): Promise<CommandHandler> {
+		await super.loadAll(directory);
+		// TODO: WIll need to update the global registration logic
+		// Filter all global commands with the names of the registered commands.
+		// For any that aren't registered, register them each individually instead
+		const registeredCommands = await this.getGlobalCommands();
+		const globalCommands = this.modules.filter((command) => command.scope === 'global');
+		if (registeredCommands.length === globalCommands.size) return this;
+		// TODO: guild restricted logic
+		// Guild restricted commands should be registered against a list of guilds in the DB to load them for.
+		// const guildCommands = this.modules.filter(command => command.scope !== 'global');
+		const promises: Promise<unknown>[] = [];
+		promises.push(this.registerCommandsGlobally(Array.from(globalCommands.values())));
+		await Promise.all(promises);
+		return this;
+	}
 
-    // TODO: Deregister functions
+	// TODO: Deregister functions
 
-    public async handle(interaction: CommandInteraction): Promise<boolean | null> {
-        let command: Command = null;
-        try {
-        const { commandName } = interaction;
-        if (!this.modules.has(commandName)) return;
+	public async handle(interaction: CommandInteraction): Promise<boolean | null> {
+		const { commandName } = interaction;
+		if (!this.modules.has(commandName)) return false;
 
-        command = this.modules.get(commandName);
-        if (command.shouldDefer)
-            await interaction.deferReply();
-        if (await this.runCommandInhibitors(interaction, command)) {
-            return false;
-        }
-        if (await this.runInhibitors(interaction, command))
-            return false;
-        if (await command.shouldExecute(interaction)) {
-            await this.runCommand(interaction, command);
-            return true;
-        } else {
-            this.emit(CommandHandlerEvents.COMMAND_BLOCKED, { interaction, command, reason: 'shouldExecute' })
-            return false;
-        }
-        
-        } catch (error) {
-            this.emitError(error, interaction, command);
-            return null;
-        }
-    }
+		const command = this.modules.get(commandName) as Command;
 
-    public async runCommandInhibitors(interaction: CommandInteraction, command: Command) {
-        if (this.blockClient && interaction.user.id === this.client.user.id) {
-            this.emit(CommandHandlerEvents.COMMAND_BLOCKED, { interaction, command, reason: 'self'})
-        }
+		try {
+			if (command.shouldDefer) await interaction.deferReply();
+			if (await this.runCommandInhibitors(interaction, command)) {
+				return false;
+			}
+			if (await this.runInhibitors(interaction, command)) return false;
+			if (await command.shouldExecute(interaction)) {
+				await this.runCommand(interaction, command);
+				return true;
+			}
+			this.emit(CommandHandlerEvents.COMMAND_BLOCKED, { interaction, command, reason: 'shouldExecute' });
+			return false;
+		} catch (error) {
+			this.emitError(error, interaction, command);
+			return null;
+		}
+	}
 
-        if (this.blockBots && interaction.user.bot) {
-            this.emit(CommandHandlerEvents.COMMAND_BLOCKED, { interaction, command, reason: 'bot'})
-            return true;
-        }
+	public runCommandInhibitors(interaction: CommandInteraction, command: Command) {
+		if (this.blockClient && !(this.client.user === null) && interaction.user.id === this.client.user.id) {
+			this.emit(CommandHandlerEvents.COMMAND_BLOCKED, { interaction, command, reason: 'self' });
+		}
 
-        if (command.ownerOnly) {
-            const isOwner = this.client.isOwner(interaction.user);
-            if (!isOwner) {
-                this.emit(CommandHandlerEvents.COMMAND_BLOCKED, { interaction, command, reason: 'owner' });
-                return true;
-            }
-        }
+		if (this.blockBots && interaction.user.bot) {
+			this.emit(CommandHandlerEvents.COMMAND_BLOCKED, { interaction, command, reason: 'bot' });
+			return true;
+		}
 
-        if (command.channels.size > 0 && !command.channels.has(interaction.channel.type)) {
-            this.emit(CommandHandlerEvents.COMMAND_BLOCKED, { interaction, command, reason: 'channel' });
-            return true;
-        }
+		if (command.ownerOnly) {
+			const isOwner = this.client.isOwner(interaction.user);
+			if (!isOwner) {
+				this.emit(CommandHandlerEvents.COMMAND_BLOCKED, { interaction, command, reason: 'owner' });
+				return true;
+			}
+		}
 
-        if (this.runCooldowns(interaction, command)) {
-            return true;
-        }
+		if (
+			(command.channels.size > 0 && interaction.channel === null) ||
+			(interaction.channel !== null && !command.channels.has(interaction.channel.type))
+		) {
+			this.emit(CommandHandlerEvents.COMMAND_BLOCKED, { interaction, command, reason: 'channel' });
+			return true;
+		}
 
-        return false;
-    }
+		if (this.runCooldowns(interaction, command)) {
+			return true;
+		}
 
-    public async runInhibitors(interaction: CommandInteraction, command: Command) {
-        const reason = this.inhibitorHandler
-            ? await this.inhibitorHandler.test(interaction, command)
-            : null;
+		return false;
+	}
 
-        if (reason !== null) {
-            this.emit(CommandHandlerEvents.COMMAND_BLOCKED, { interaction, command, reason});
-            return true;
-        }
+	public async runInhibitors(interaction: CommandInteraction, command: Command) {
+		const reason = this.inhibitorHandler ? await this.inhibitorHandler.test(interaction, command) : null;
 
-        return false;
-    }
+		if (reason !== null) {
+			this.emit(CommandHandlerEvents.COMMAND_BLOCKED, { interaction, command, reason });
+			return true;
+		}
 
-    public runCooldowns(interaction: CommandInteraction, command: Command): boolean {
-        const ignorer = command.ignoreCooldown || this.ignoreCooldown;
-        const isIgnored = Array.isArray(ignorer)
-            ? ignorer.includes(interaction.user.id)
-            : ignorer(interaction, command);
+		return false;
+	}
 
-        if (isIgnored) return false;
+	public runCooldowns(interaction: CommandInteraction, command: Command): boolean {
+		const ignorer = command.ignoreCooldown ?? this.ignoreCooldown;
+		const isIgnored = Array.isArray(ignorer) ? ignorer.includes(interaction.user.id) : ignorer(interaction, command);
+		// Store just the id string so the entire command object reference
+		// isn't kept in the setTimeout below
+		const commandId = command.id;
+		if (isIgnored) return false;
 
-        const time = command.cooldown != null ? command.cooldown : this.defaultCooldown;
-        if (!time) return false;
+		const time = command.cooldown >= 0 ? command.cooldown : this.defaultCooldown;
+		if (!time) return false;
 
-        const endTime = interaction.createdTimestamp + time;
+		const endTime = interaction.createdTimestamp + time;
 
-        const userId = interaction.user.id;
-        if (!this.cooldowns.has(userId)) this.cooldowns.set(userId, {});
+		const userId = interaction.user.id;
+		// If user cooldowns don't exist create them.
+		if (!this.cooldowns.has(userId)) this.cooldowns.set(userId, new Collection());
 
-        if (!this.cooldowns.get(userId)[command.id]) {
-            this.cooldowns.get(userId)[command.id] = {
-                timer: setTimeout(() => {
-                    if (this.cooldowns.get(userId)[command.id]) {
-                        clearTimeout(this.cooldowns.get(userId)[command.id].timer);
-                    }
-                    delete this.cooldowns.get(userId)[command.id];
+		const userCooldowns = this.cooldowns.get(userId) as Collection<string, CooldownData>;
+		// If a cooldown for the command doesn't exist yet create it.
+		if (!userCooldowns.has(commandId)) {
+			userCooldowns.set(commandId, {
+				timer: setTimeout(() => {
+					if (userCooldowns.has(commandId)) {
+						const commandCooldownData = userCooldowns.get(commandId);
+						if (commandCooldownData) clearTimeout(commandCooldownData.timer);
+					}
+					userCooldowns.get(command.id);
 
-                    if (Object.keys(this.cooldowns.get(userId)).length === 0) {
-                        this.cooldowns.delete(userId);
-                    }
-                }, time),
-                end: endTime,
-                uses: 0
-            };
-        }
+					if (userCooldowns.size === 0) {
+						this.cooldowns.delete(userId);
+					}
+				}, time),
+				end: endTime,
+				uses: 0,
+			});
+		}
 
-        const entry = this.cooldowns.get(userId)[command.id];
+		const entry = userCooldowns.get(commandId) as CooldownData;
 
-        if (entry.uses >= command.ratelimit) {
-            const end = this.cooldowns.get(userId)[command.id].end;
-            const diff = end - interaction.createdTimestamp;
+		if (entry.uses >= command.ratelimit) {
+			const diff = entry.end - interaction.createdTimestamp;
 
-            this.emit(CommandHandlerEvents.COOLDOWN, { interaction, command, remainingTime: diff });
-            return true;
-        }
+			this.emit(CommandHandlerEvents.COOLDOWN, { interaction, command, remainingTime: diff });
+			return true;
+		}
 
-        entry.uses++;
-        return false;
-    }
+		entry.uses++;
+		return false;
+	}
 
-    public async runCommand(interaction: CommandInteraction, command: Command): Promise<void> {
-        this.emit(CommandHandlerEvents.STARTED, { interaction, command });
-        const result = await command.execute(interaction);
-        this.emit(CommandHandlerEvents.ENDED, { interaction, command, result });
-    }
+	public async runCommand(interaction: CommandInteraction, command: Command): Promise<void> {
+		this.emit(CommandHandlerEvents.STARTED, { interaction, command });
+		const result = await command.execute(interaction);
+		this.emit(CommandHandlerEvents.ENDED, { interaction, command, result });
+	}
 
-    public useInhibitorHandler(inhibitorHandler: InhibitorHandler): CommandHandler {
-        this.inhibitorHandler = inhibitorHandler;
-        return this;
-    }
+	public useInhibitorHandler(inhibitorHandler: InhibitorHandler): CommandHandler {
+		this.inhibitorHandler = inhibitorHandler;
+		return this;
+	}
 
-    public useListenerHandler(listenerHandler: ListenerHandler): CommandHandler {
-        this.listenerHandler = listenerHandler;
-        return this;
-    }
+	public useListenerHandler(listenerHandler: ListenerHandler): CommandHandler {
+		this.listenerHandler = listenerHandler;
+		return this;
+	}
 
-    
-    public emitError(error: Error, interaction: Interaction, command: Command) {
-        if (this.listenerCount(CommandHandlerEvents.ERROR)) {
-            this.emit(CommandHandlerEvents.ERROR, { error, interaction, command });
-            return;
-        }
+	public emitError(error: Error, interaction: Interaction, command: Command) {
+		if (this.listenerCount(CommandHandlerEvents.ERROR)) {
+			this.emit(CommandHandlerEvents.ERROR, { error, interaction, command });
+			return;
+		}
 
-        throw error;
-    }
+		throw error;
+	}
 
-    private commandsToData(commands: Command[]) {
-        return commands.map(command => command.data.toJSON());
-    }
-};
+	private commandsToData(commands: Command[]) {
+		return commands.map((command) => command.data.toJSON());
+	}
+}
