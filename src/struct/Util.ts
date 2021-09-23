1dcdb4de988d0d772b5e1c00c0966348de315afc
psibean <psionatix@gmail.com>
2021-09-23 23:10:22 +0930

    Fix Util

--- src/struct/Util.ts
+++ src/struct/Util.ts
@@ -1,40 +1,41 @@
 export function isPromise(value: any): boolean {
-    return value !== null
-      && value !== undefined
-      && typeof value.then === 'function'
-      && typeof value.catch === 'function';
-};
+	return value !== null && value !== undefined && typeof value.then === 'function' && typeof value.catch === 'function';
+}
 
 export const CommandHandlerEvents = {
-  COMMAND_BLOCKED: 'blocked',
-  COOLDOWN: 'cooldown',
-  ENDED: 'ended',
-  ERROR: 'error',
-  STARTED: 'started',
+	COMMAND_BLOCKED: 'blocked',
+	COOLDOWN: 'cooldown',
+	ENDED: 'ended',
+	ERROR: 'error',
+	STARTED: 'started',
 };
 
 export const BaseHandlerEvents = {
-  LOAD: 'load',
-  REMOVE: 'remove',
+	LOAD: 'load',
+	REMOVE: 'remove',
 };
 
 export const ErrorMessages = {
-  // Module-related
-  FILE_NOT_FOUND: (filename: string) => `File '${filename}' not found`,
-  MODULE_NOT_FOUND: (constructor: string, id: string) => `${constructor} '${id}' does not exist`,
-  ALREADY_LOADED: (constructor: string, id: string) => `${constructor} '${id}' is already loaded`,
-  NOT_RELOADABLE: (constructor: string, id: string) => `${constructor} '${id}' is not reloadable`,
-  INVALID_CLASS_TO_HANDLE: (given: string, expected: string) => `Class to handle ${given} is not a subclass of ${expected}`,
+	// Module-related
+	FILE_NOT_FOUND: (filename: string) => `File '${filename}' not found`,
+	MODULE_NOT_FOUND: (constructor: string, id: string) => `${constructor} '${id}' does not exist`,
+	ALREADY_LOADED: (constructor: string, id: string) => `${constructor} '${id}' is already loaded`,
+	NOT_RELOADABLE: (constructor: string, id: string) => `${constructor} '${id}' is not reloadable`,
+	INVALID_CLASS_TO_HANDLE: (given: string, expected: string) =>
+		`Class to handle ${given} is not a subclass of ${expected}`,
 
-  // Command-related
-  ALIAS_CONFLICT: (alias: string, id: string, conflict: string) => `Alias '${alias}' of '${id}' already exists on '${conflict}'`,
+	// Command-related
+	ALIAS_CONFLICT: (alias: string, id: string, conflict: string) =>
+		`Alias '${alias}' of '${id}' already exists on '${conflict}'`,
 
-  // Options-related
-  COMMAND_UTIL_EXPLICIT: 'The command handler options `handleEdits` and `storeMessages` require the `commandUtil` option to be true',
-  UNKNOWN_MATCH_TYPE: match => `Unknown match type '${match}'`,
+	// Options-related
+	COMMAND_UTIL_EXPLICIT:
+		'The command handler options `handleEdits` and `storeMessages` require the `commandUtil` option to be true',
+	UNKNOWN_MATCH_TYPE: (match) => `Unknown match type '${match}'`,
 
-  // Generic errors
-  NOT_INSTANTIABLE: (constructor: string) => `${constructor} is not instantiable`,
-  NOT_IMPLEMENTED: (constructor: string, method: string) => `${constructor}#${method} has not been implemented`,
-  INVALID_TYPE: (name: string, expected: string, vowel: boolean = false) => `Value of '${name}' was not ${vowel ? 'an' : 'a'} ${expected}`
+	// Generic errors
+	NOT_INSTANTIABLE: (constructor: string) => `${constructor} is not instantiable`,
+	NOT_IMPLEMENTED: (constructor: string, method: string) => `${constructor}#${method} has not been implemented`,
+	INVALID_TYPE: (name: string, expected: string, vowel = false) =>
+		`Value of '${name}' was not ${vowel ? 'an' : 'a'} ${expected}`,
 };
