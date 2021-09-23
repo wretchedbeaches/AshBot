4638704a8e18b06e76c443ac6eeb53126cb66263
psibean <psionatix@gmail.com>
2021-09-23 22:51:53 +0930

    Update inhibitor and inhibitor handler

--- src/struct/inhibitors/Inhibitor.ts
+++ src/struct/inhibitors/Inhibitor.ts
@@ -1,28 +1,29 @@
-import { Interaction } from "discord.js";
-import BaseModule, { BaseModuleOptions } from "../BaseModule";
-import Command from "../commands/Command";
-import { ErrorMessages } from "../Util";
-import InhibitorHandler from "./InhibitorHandler";
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { Interaction } from 'discord.js';
+import BaseModule, { BaseModuleOptions } from '../BaseModule';
+import Command from '../commands/Command';
+import { ErrorMessages } from '../Util';
+import InhibitorHandler from './InhibitorHandler';
 
 interface InhibitorOptions extends BaseModuleOptions {
-  reason: string;
-  priority: number;
+	reason: string;
+	priority: number;
 }
 
 export default class Inhibitor extends BaseModule {
-  public id: string;
-  public reason: string;
-  public priority: number;
-  public handler: InhibitorHandler;
+	public id: string;
+	public reason: string;
+	public priority: number;
+	public handler: InhibitorHandler;
 
-  public constructor(id: string, { reason, priority = 0, ...rest }: InhibitorOptions) {
-    super(id, rest);
-    this.id = id;
-    this.reason = reason;
-    this.priority = priority;
-  }
+	public constructor(id: string, { reason, priority = 0, ...rest }: InhibitorOptions) {
+		super(id, rest);
+		this.id = id;
+		this.reason = reason;
+		this.priority = priority;
+	}
 
-  public execute(interaction: Interaction, command: Command): boolean | Promise<boolean> {
-    throw new Error(ErrorMessages.NOT_IMPLEMENTED(this.constructor.name, 'execute'));
-  }
-};
+	public execute(interaction: Interaction, command: Command): boolean | Promise<boolean> {
+		throw new Error(ErrorMessages.NOT_IMPLEMENTED(this.constructor.name, 'execute'));
+	}
+}
--- src/struct/inhibitors/InhibitorHandler.ts
+++ src/struct/inhibitors/InhibitorHandler.ts
@@ -1,36 +1,44 @@
-import { Collection, Interaction } from 'discord.js';
+import { Collection, CommandInteraction } from 'discord.js';
 import BaseHandler, { BaseHandlerOptions } from '../BaseHandler';
 import Inhibitor from './Inhibitor';
-import { ErrorMessages, isPromise } from '../Util';
 import Command from '../commands/Command';
+import { isPromise } from '../Util';
 
 export default class InhibitorHandler extends BaseHandler {
-    public modules: Collection<string, Inhibitor>;
-    public classToHandle: new (...args: any[]) => Inhibitor;
+	public modules: Collection<string, Inhibitor>;
+	public classToHandle: new (...args: any[]) => Inhibitor;
 
-    public constructor(client, options: BaseHandlerOptions) {
-        super(client, options);
-        this.classToHandle = Inhibitor;
-    }
+	public constructor(client, options: BaseHandlerOptions) {
+		super(client, options);
+		this.classToHandle = Inhibitor;
+	}
 
-    public async test(interaction: Interaction, command: Command): Promise<string|void> {
-        if (!this.modules.size) return null;
+	private async runInhibitor(
+		inhibitor: Inhibitor,
+		interaction: CommandInteraction,
+		command: Command,
+	): Promise<Inhibitor | null> {
+		let inhibited = inhibitor.execute(interaction, command);
+		if (isPromise(inhibited)) inhibited = await inhibited;
+		if (inhibited) return inhibitor;
+		return null;
+	}
 
-        const promises = [];
-        // TODO: assign an inhibitor a command cetagory and filter here?
-        for (const inhibitor of this.modules.values()) {
-            promises.push((async () => {
-                let inhibited = inhibitor.execute(interaction, command);
-                if (isPromise(inhibited)) inhibited = await inhibited;
-                if (inhibited) return inhibitor;
-                return null;
-            })());
-        }
+	public async test(interaction: CommandInteraction, command: Command): Promise<string | void> {
+		if (!this.modules.size) return;
 
-        const inhibitedInhibitors = (await Promise.all(promises)).filter(r => r);
-        if (!inhibitedInhibitors.length) return null;
+		const promises: Promise<Inhibitor | null>[] = [];
+		// TODO: assign an inhibitor a command cetagory and filter here?
+		for (const inhibitor of this.modules.values()) {
+			promises.push(this.runInhibitor(inhibitor, interaction, command));
+		}
 
-        inhibitedInhibitors.sort((a, b) => b.priority - a.priority);
-        return inhibitedInhibitors[0].reason;
-    }
-};
+		const inhibitedInhibitors = (await Promise.all(promises)).filter((r) => r);
+		if (inhibitedInhibitors.length === 0) return;
+		if (inhibitedInhibitors.length >= 2) {
+			inhibitedInhibitors.sort((a, b) => (b?.priority ?? 0) - (a?.priority ?? 0));
+		}
+
+		return inhibitedInhibitors[0]?.reason;
+	}
+}
