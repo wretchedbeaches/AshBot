00fc252e2940a78edfc3187c3e8531d40498bef4
psibean <psionatix@gmail.com>
2021-09-23 23:09:49 +0930

    Fix BaseModule

--- src/struct/BaseModule.ts
+++ src/struct/BaseModule.ts
@@ -1,46 +1,47 @@
-import { Client } from "discord.js";
-import BaseClient from "../client/BotClient";
-import BaseHandler from "./BaseHandler";
-import Category from "./Category";
+import BaseClient from '../client/BotClient';
+import BaseHandler from './BaseHandler';
+import Category from './Category';
 
 export interface BaseModuleOptions {
-  category?: string;
+	category?: string;
 }
 
 export interface BaseModuleAttributes {
-  client: BaseClient;
-  id: string;
-  categoryID: string;
-  category: Category;
-  filepath: string;
-  handler: BaseHandler;
+	client: BaseClient | null;
+	id: string;
+	categoryID: string;
+	category?: Category;
+	filepath: string | null;
+	handler: BaseHandler | null;
 }
 
 export default class BaseModule implements BaseModuleAttributes {
-  public client: BaseClient;
-  public id: string;
-  public categoryID: string;
-  public category: Category;
-  public filepath: string;
-  public handler: BaseHandler;
+	public client: BaseClient | null;
+	public id: string;
+	public categoryID: string;
+	public category: Category;
+	public filepath: string | null;
+	public handler: BaseHandler | null;
 
-  public constructor(id: string, { category = 'default' }: BaseModuleOptions) {
-    this.id = id;
-    this.categoryID = category;
-    this.filepath = null;
-    this.handler = null;
-    this.client = null;
-  }
+	public constructor(id: string, { category = 'default' }: BaseModuleOptions) {
+		this.id = id;
+		this.categoryID = category;
+		this.filepath = null;
+		this.handler = null;
+		this.client = null;
+	}
 
-  public reload(): BaseModule {
-    return this.handler.reload(this.id);
-  }
+	public reload(): BaseModule | null {
+		if (this.handler === null) return null;
+		return this.handler.reload(this.id);
+	}
 
-  public remove(): BaseModule {
-    return this.handler.remove(this.id);
-  }
+	public remove(): BaseModule | null {
+		if (this.handler === null) return null;
+		return this.handler.remove(this.id);
+	}
 
-  public toString(): string {
-    return this.id;
-  }
+	public toString(): string {
+		return this.id;
+	}
 }
