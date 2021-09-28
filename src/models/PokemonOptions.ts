6bec962348899d6efdfc3146830f56c4b43476ac
psibean <psionatix@gmail.com>
2021-09-28 22:27:25 +0930

    More lint error fixing

--- src/models/PokemonOptions.ts
+++ src/models/PokemonOptions.ts
@@ -1,15 +1,16 @@
 export default interface PokesetOptions {
-  type: 'pokemon';
-  boosted?: boolean;
-  name?: string;
-  geofilter?: string | Object;
-  miniv?: number;
-  maxiv?: number;
-  mincp?: number;
-  maxcp?: number;
-  minlevel?: number;
-  maxlevel?: number;
-  rawiv?: string;
-  rmchannel?: boolean;
-  train?: boolean;
+	type: 'pokemon';
+	boosted?: boolean;
+	name?: string;
+	// eslint-disable-next-line @typescript-eslint/ban-types
+	geofilter?: string | Object;
+	miniv?: number;
+	maxiv?: number;
+	mincp?: number;
+	maxcp?: number;
+	minlevel?: number;
+	maxlevel?: number;
+	rawiv?: string;
+	rmchannel?: boolean;
+	train?: boolean;
 }
--- src/models/RaidOptions.ts
+++ src/models/RaidOptions.ts
@@ -1,13 +1,13 @@
 export default interface PokesetOptions {
-  type: 'raid';
-  ex?: boolean;
-  name?: string;
-  geofilter?: string;
-  team?: 'uncontested' | 'mystic' | 'valor' | 'instinct';
-  mincp?: number;
-  maxcp?: number;
-  minlevel?: number;
-  maxlevel?: number;
-  rmchannel?: boolean;
-  train?: boolean;
+	type: 'raid';
+	ex?: boolean;
+	name?: string;
+	geofilter?: string;
+	team?: 'uncontested' | 'mystic' | 'valor' | 'instinct';
+	mincp?: number;
+	maxcp?: number;
+	minlevel?: number;
+	maxlevel?: number;
+	rmchannel?: boolean;
+	train?: boolean;
 }
