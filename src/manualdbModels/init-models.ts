dbea34253cfc4e51fc6af848bad64cb9c906e519
psibean <psionatix@gmail.com>
2021-10-02 17:55:31 +0930

    Fix lint issues with manualdbModels

--- src/manualdbModels/init-models.ts
+++ src/manualdbModels/init-models.ts
@@ -1,49 +1,43 @@
-import type { Sequelize, Model } from "sequelize";
-import { communities } from "./communities";
-import type { communitiesAttributes, communitiesCreationAttributes } from "./communities";
-import { ingress_portals } from "./ingress_portals";
-import type { ingress_portalsAttributes, ingress_portalsCreationAttributes } from "./ingress_portals";
-import { nests } from "./nests";
-import type { nestsAttributes, nestsCreationAttributes } from "./nests";
-import { poi } from "./poi";
-import type { poiAttributes, poiCreationAttributes } from "./poi";
-import { users } from "./users";
-import type { usersAttributes, usersCreationAttributes } from "./users";
+/* eslint-disable no-duplicate-imports */
+import type { Sequelize } from 'sequelize';
+import { communities } from './communities';
+import type { communitiesAttributes, communitiesCreationAttributes } from './communities';
+import { ingress_portals } from './ingress_portals';
+import type { ingress_portalsAttributes, ingress_portalsCreationAttributes } from './ingress_portals';
+import { nests } from './nests';
+import type { nestsAttributes, nestsCreationAttributes } from './nests';
+import { poi } from './poi';
+import type { poiAttributes, poiCreationAttributes } from './poi';
+import { users } from './users';
+import type { usersAttributes, usersCreationAttributes } from './users';
 
-export {
-  communities,
-  ingress_portals,
-  nests,
-  poi,
-  users,
-};
+export { communities, ingress_portals, nests, poi, users };
 
 export type {
-  communitiesAttributes,
-  communitiesCreationAttributes,
-  ingress_portalsAttributes,
-  ingress_portalsCreationAttributes,
-  nestsAttributes,
-  nestsCreationAttributes,
-  poiAttributes,
-  poiCreationAttributes,
-  usersAttributes,
-  usersCreationAttributes,
+	communitiesAttributes,
+	communitiesCreationAttributes,
+	ingress_portalsAttributes,
+	ingress_portalsCreationAttributes,
+	nestsAttributes,
+	nestsCreationAttributes,
+	poiAttributes,
+	poiCreationAttributes,
+	usersAttributes,
+	usersCreationAttributes,
 };
 
 export function initModels(sequelize: Sequelize) {
-  communities.initModel(sequelize);
-  ingress_portals.initModel(sequelize);
-  nests.initModel(sequelize);
-  poi.initModel(sequelize);
-  users.initModel(sequelize);
+	communities.initModel(sequelize);
+	ingress_portals.initModel(sequelize);
+	nests.initModel(sequelize);
+	poi.initModel(sequelize);
+	users.initModel(sequelize);
 
-
-  return {
-    communities: communities,
-    ingress_portals: ingress_portals,
-    nests: nests,
-    poi: poi,
-    users: users,
-  };
+	return {
+		communities: communities,
+		ingress_portals: ingress_portals,
+		nests: nests,
+		poi: poi,
+		users: users,
+	};
 }
