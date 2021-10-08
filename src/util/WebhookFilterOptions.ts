8f41f2b1b0538631f5e7b6f9bbf5e05e749475cd
psibean <psionatix@gmail.com>
2021-10-09 01:00:36 +1030

    Add geofilter parser

--- src/util/WebhookFilterOptions.ts
+++ src/util/WebhookFilterOptions.ts
@@ -1,4 +1,42 @@
 import { SlashCommandBuilder, SlashCommandSubcommandBuilder } from '@discordjs/builders';
+import { CommandInteraction } from 'discord.js';
+import config from '../config.json';
+
+export const parseGeofilterOptions = (interaction: CommandInteraction, channelConfiguration, errorSuffix: string) => {
+	let error = '';
+	const cityArgument = interaction.options.getString('city', false);
+	// If a city is provided it shall take priority.
+	if (cityArgument === null) {
+		const radiusArgument = interaction.options.getNumber('radius', false);
+		const latitudeArgument = interaction.options.getNumber('latitude', false);
+		const longitudeArgument = interaction.options.getNumber('longitude', false);
+		const hasLatLong = latitudeArgument && longitudeArgument;
+
+		const attemptedToSetGeofilter =
+			radiusArgument !== null && (latitudeArgument !== null || longitudeArgument !== null);
+		if (radiusArgument !== null) {
+			const unitArgument = interaction.options.getString('unit') as 'km' | 'm' | null;
+			if (unitArgument === null) {
+				error = `A radius of '${radiusArgument}' was provided without a unit (m or km), geofilter was not ${errorSuffix}.`;
+			} else if (hasLatLong) {
+				channelConfiguration.geofilter = {
+					center: [latitudeArgument, longitudeArgument!],
+					radius: radiusArgument,
+					unit: unitArgument,
+				};
+			}
+		} else if (attemptedToSetGeofilter) {
+			error = `The following arguments were provided for the geo filter:`;
+			error += `\nlat/long: '${latitudeArgument ?? ''}/${longitudeArgument ?? ''}'`;
+			error += `\n\n**But no radius was provided and is required when providing a lat/long.**\nGeofilter was not ${errorSuffix}`;
+		}
+	} else if (config.cities[cityArgument.toLowerCase()]) {
+		channelConfiguration.geofilter = cityArgument.toLowerCase();
+	} else {
+		error = `City '${cityArgument}' was provided but could not be found, geofilter was not ${errorSuffix}.`;
+	}
+	return error;
+};
 
 export const addGeofilterOptions = (command: SlashCommandBuilder | SlashCommandSubcommandBuilder) => {
 	command
