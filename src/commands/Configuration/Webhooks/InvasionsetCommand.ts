12b7c694162cda35fe38f11d601fbce6d5ca1543
psibean <psionatix@gmail.com>
2021-10-09 00:42:55 +1030

    Update InvasionsetCommand

--- src/commands/Configuration/Webhooks/InvasionsetCommand.ts
+++ src/commands/Configuration/Webhooks/InvasionsetCommand.ts
@@ -1,163 +1,74 @@
-import { Command } from 'discord-akairo';
-import { Message, TextChannel } from 'discord.js';
+import { CommandInteraction } from 'discord.js';
 import { stripIndents } from 'common-tags';
-import config from '../../../config.json';
+import Command from '../../../struct/commands/Command';
+import { addGeofilterOptions, parseGeofilterOptions } from '../../../util/WebhookFilterOptions';
+import { InvasionsetConfig } from '../../../models/WebhookConfigurations';
 
-export default class invasionset extends Command {
-  public constructor() {
-    super('invasionset', {
-      aliases: ['invasionset', 'invasion'],
-      category: 'Webhooks',
-      description: {
-        content:
-          'Set or remove the invasion webhook configuration for a particular channel.',
-        usage: stripIndents`invasionset
+export default class InvasionSetCommand extends Command {
+	public constructor() {
+		super('invasionset', {
+			description: {
+				content: 'Set or remove the invasion webhook configuration for a particular channel.',
+				usage: stripIndents`invasionset
         leader \`true|false\`
         geofilter \`distance(km/m) lattitude,longitude|city\`
-        train
+        train`,
+			},
+			category: 'Webhooks',
+			ratelimit: 3,
+		});
 
-        **OR**
-        
-        invasionset rmchannel`,
-        examples: ['invasionset leader true', 'invasionset rmchannel'],
-      },
-      ratelimit: 3,
-      args: [
-        {
-          id: 'rmchannel',
-          type: 'string',
-          flag: ['rmchannel'],
-          match: 'flag',
-        },
-        {
-          id: 'leader',
-          type: (_: Message, str: string): null | boolean => {
-            if (['true', 'false'].includes(str)) return str === 'true';
-            return null;
-          },
-          flag: ['leader'],
-          prompt: {
-            optional: true,
-            start: (msg: Message) =>
-              `${msg.author}, please provide a valid leader value, either **true** or **false**.`,
-            retry: (msg: Message) =>
-              `${msg.author}, please provide a valid leader value, either **true** or **false**.`,
-          },
+		this.data
+			.addChannelOption((option) =>
+				option.setName('channel').setDescription('The channel to create the raid webhook for.').setRequired(false),
+			)
+			.addBooleanOption((updateOption) =>
+				updateOption
+					.setName('update')
+					.setDescription('If true, existing config will be updated instead of overwritten.'),
+			)
+			.addBooleanOption((leaderOption) =>
+				leaderOption.setName('leader').setDescription('Whether or not to filter on leaders.'),
+			)
+			.addBooleanOption((trainOption) =>
+				trainOption.setName('train').setDescription('Whether or not to filter on train.'),
+			);
 
-          match: 'option',
-        },
-        {
-          id: 'train',
-          type: (_: Message, str: string): null | boolean => {
-            if (['true', 'false'].includes(str)) return str === 'true';
-            return null;
-          },
-          flag: ['train'],
-          match: 'flag',
-        },
-        {
-          id: 'geofilter',
-          type: (_: Message, str: string): null | string | Object => {
-            try {
-              if (str.startsWith('geofilter')) str = str.substring(10);
-              if (Object.keys(config.cities).includes(str.toLowerCase()))
-                return str;
-              const parts = str.split(' ');
-              if (parts[0].endsWith('km') || parts[0].endsWith('m')) {
-                return {
-                  center: [
-                    Number(parts[1].split(',')[0]),
-                    Number(parts[1].split(',')[1]),
-                  ],
-                  radius: parts[0].endsWith('km')
-                    ? Number(parts[0].substring(0, parts[0].indexOf('km')))
-                    : Number(parts[0].substring(0, parts[0].indexOf('m'))),
-                };
-              }
-              return null;
-            } catch (e) {
-              return null;
-            }
-          },
-          prompt: {
-            optional: true,
-            start: (msg: Message) =>
-              `${msg.author}, please provide a valid geofilter. Geofilters can either be in the format \`distance\` \`km or m\` \`lattitude\`,\`longitude\` or a city.`,
-            retry: (msg: Message) =>
-              `${msg.author}, please provide a valid geofilter. Geofilters can either be in the format \`distance\` \`km or m\` \`lattitude\`,\`longitude\` or a city.`,
-          },
-          match: 'rest',
-        },
-      ],
-    });
-  }
+		addGeofilterOptions(this.data);
+	}
 
-  public async exec(message: Message, args: any) {
-    if (!this.client.handleAdminPermissions(message)) return;
-    if (args.rmchannel) {
-      const settings = this.client.settings.get(
-        message.guild.id,
-        'channels',
-        {}
-      );
-      delete settings[message.channel.id];
-      await this.client.settings.set(message.guild.id, 'channels', settings);
-      if (this.client.intervals[message.channel.id]) {
-        clearInterval(this.client.intervals[message.channel.id]);
-        delete this.client.intervals[message.channel.id];
-        delete this.client.embedQueue[message.channel.id];
-        delete this.client.trains[message.channel.id];
-      }
-      return message.util.send(
-        "Successfully removed channel's invasion webhook configuration."
-      );
-    } else {
-      const settings = this.client.settings.get(
-        message.guild.id,
-        'channels',
-        {}
-      );
-      for (let arg in args)
-        if (args[arg] === null || arg === 'rmchannel') delete args[arg];
-      args.type = 'invasion';
-      settings[message.channel.id] = {
-        ...settings[message.channel.id],
-        ...args,
-      };
-      await this.client.settings.set(message.guild.id, 'channels', settings);
-      if (this.client.intervals[message.channel.id])
-        clearInterval(this.client.intervals[message.channel.id]);
-      this.client.embedQueue[message.channel.id] = [];
-      this.client.intervals[message.channel.id] = setInterval(async () => {
-        if (
-          this.client.embedQueue[message.channel.id] &&
-          this.client.embedQueue[message.channel.id].length > 0
-        ) {
-          let channel = this.client.channels.cache.get(message.channel.id);
-          if (!channel)
-            channel = await this.client.channels.fetch(message.channel.id);
-          if (
-            !((channel): channel is TextChannel => channel.type === 'text')(
-              channel
-            )
-          )
-            return;
-          const embed = this.client.embedQueue[message.channel.id][0];
-          channel.send({
-            content: embed.coordinates
-              ? `${embed.coordinates[0]}, ${embed.coordinates[1]}`
-              : embed.message,
-            embed: embed.embed,
-          });
-          this.client.embedQueue[message.channel.id] = this.client.embedQueue[
-            message.channel.id
-          ].slice(1, this.client.embedQueue[message.channel.id].length);
-        }
-      }, Number(process.env.QUEUE_INTERVAL));
-      delete this.client.trains[message.channel.id];
-      return message.util.send(
-        "Successfully updated channel's invasion webhook configuration."
-      );
-    }
-  }
+	public async execute(interaction: CommandInteraction) {
+		const isUpdate = interaction.options.getBoolean('update', false) ?? false;
+		// TODO: once https://github.com/discordjs/builders/pull/41 is merged
+		// Only allow for only text channels to be selected for this argument.
+		const channel = interaction.options.getChannel('channel', false) ?? interaction.channel;
+		const guildId = interaction.guildId;
+		// TODO: Remove this check once command is forced to be guild only.
+		if (guildId == null || channel === null) {
+			return interaction.editReply(
+				`No channel was provided for the Raid Webhook Configuration and the command was not used in a channel.`,
+			);
+		}
+		const channelId = channel.id;
+		const channelConfigurations = this.client.settings.get(guildId, 'channels', {});
+		// If set to update and the configuration already exists, update it.
+		const channelConfiguration: InvasionsetConfig =
+			isUpdate && channelConfigurations[channelId] !== undefined ? channelConfigurations[channelId] : { type: 'raid' };
+
+		const filterErrorSuffix = isUpdate ? 'updated' : 'set';
+		const error = parseGeofilterOptions(interaction, channelConfiguration, filterErrorSuffix);
+
+		const leaderArgument = interaction.options.getBoolean('leader', false);
+		const trainArgument = interaction.options.getBoolean('train', false);
+		if (leaderArgument !== null) channelConfiguration.leader = leaderArgument;
+		if (trainArgument !== null) channelConfiguration.train = trainArgument;
+		channelConfigurations[channelId] = channelConfiguration;
+		if (this.client.intervals.has(channelId)) clearInterval(this.client.intervals.get(channelId)!);
+		await this.client.settings.set(guildId, 'channels', channelConfigurations);
+		this.client.embedQueue.set(channelId, []);
+		this.client.setInterval(channelId);
+		this.client.trains.delete(channelId);
+		const finalError = error === '' ? '' : ` However the following errors occurred:\n\n${error.trim()}`;
+		return interaction.editReply(`Successfully updated channel's raid webhook configuration.${finalError}`);
+	}
 }
