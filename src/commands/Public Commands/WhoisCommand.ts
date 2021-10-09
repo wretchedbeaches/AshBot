554349b9251848318475066d1b9c629eb867116c
psibean <psionatix@gmail.com>
2021-10-09 14:09:15 +1030

    Update WhoIsCommand

--- src/commands/Public Commands/WhoisCommand.ts
+++ src/commands/Public Commands/WhoisCommand.ts
@@ -1,56 +1,54 @@
-import { Command } from 'discord-akairo';
-import { Message, MessageEmbed, GuildMember } from 'discord.js';
+import { MessageEmbed, Permissions, CommandInteraction } from 'discord.js';
 import { stripIndents } from 'common-tags';
 import * as moment from 'moment';
 import 'moment-duration-format';
+import Command from '../../struct/commands/Command';
 
 export default class WhoIsCommand extends Command {
-    public constructor() {
-        super('whois', {
-            aliases: ['whois', 'user' , 'member', 'user-info'],
-            description: {
-                content: 'Gets info about a member.',
-                usage: '[member]',
-                examples: ['whois Meatloaf Dispenser', 'user @BEACH', 'member 463869229850951680']
-            },
-            category: 'Utilities',
-            channel: 'guild',
-            clientPermissions: ['MANAGE_MESSAGES'],
-            ratelimit: 2,
-            args: [
-                {
-                    id: 'member',
-                    match: 'content',
-                    type: 'member',
-                    default: (message: Message): GuildMember => message.member!
-                }
-            ]
-        });
-    }
+	public constructor() {
+		super('whois', {
+			scope: 'guild',
+			description: {
+				content: 'Gets info about a member.',
+				usage: '[member]',
+				examples: ['whois Meatloaf Dispenser', 'user @BEACH', 'member 463869229850951680'],
+			},
+			category: 'Utilities',
+			rateLimit: 2,
+			clientPermissions: [Permissions.FLAGS.MANAGE_MESSAGES],
+		});
+		this.data.addUserOption((userOption) =>
+			userOption.setName('member').setDescription('The member to get info for.').setRequired(true),
+		);
+	}
 
-    public async exec(message: Message, { member }: { member: GuildMember }): Promise<Message | Message[]> {
-        const { user } = member;
-        const embed = new MessageEmbed()
-            .setColor('#0000ff')
-            .setDescription(`Info about **${user.tag}** (ID: ${member.id})`)
-            .addField(
-                '❯ Member Details',
-                stripIndents`
-				${member.nickname == undefined ? '• No nickname' : ` • Nickname: ${member.nickname}`}
-				• Roles: ${member.roles.cache.map((roles): string => `\`${roles.name}\``).join(' ')}
-				• Joined at: ${moment.utc(member.joinedAt!).format('YYYY/MM/DD hh:mm:ss')}
-            `)
-            .addField(
-                '❯ User Details',
-                stripIndents`
-				• ID: ${member.id}
-				• Username: ${member.user.tag}
-				• Created at: ${moment.utc(user.createdAt).format('YYYY/MM/DD hh:mm:ss')}${user.bot ? '\n• Bot account' : ''}
-				• Status: ${user.presence.status.toUpperCase()}
-				• Activity: ${user.presence.activities[0] ? user.presence.activities[0].name : 'None'}
-            `)
-            .setThumbnail(user.displayAvatarURL());
-
-        return message.util!.send(embed);
-    }
+	public async execute(interaction: CommandInteraction) {
+		const userArgument = interaction.options.getUser('member', true);
+		const member = await interaction.guild?.members.fetch(userArgument.id);
+		const embed = new MessageEmbed()
+			.setColor('#0000ff')
+			.setDescription(`Info about **${userArgument.tag}** (ID: ${member!.id})`)
+			.addField(
+				'❯ Member Details',
+				stripIndents`
+				${member!.nickname === null ? '• No nickname' : ` • Nickname: ${member!.nickname}`}
+				• Roles: ${member!.roles.cache.map((roles): string => `\`${roles.name}\``).join(' ')}
+                • Status: ${member!.presence?.status.toUpperCase()}
+                • Activity: ${member!.presence?.activities[0] ? member!.presence.activities[0].name : 'None'}
+				• Joined at: ${moment.utc(member!.joinedAt).format('YYYY/MM/DD hh:mm:ss')}
+            `,
+			)
+			.addField(
+				'❯ User Details',
+				stripIndents`
+				• ID: ${userArgument.id}
+				• Username: ${userArgument.tag}
+				• Created at: ${moment.utc(userArgument.createdAt).format('YYYY/MM/DD hh:mm:ss')}${
+					userArgument.bot ? '\n• Bot account' : ''
+				}
+            `,
+			)
+			.setThumbnail(userArgument.displayAvatarURL());
+		return interaction.editReply({ embeds: [embed] });
+	}
 }
