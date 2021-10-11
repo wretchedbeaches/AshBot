2e8170e6779229fca833c06d665ecf17e07fb166
psibean <psionatix@gmail.com>
2021-10-11 21:15:52 +1030

    Remove path argument from express.use

--- src/client/BotClient.ts
+++ src/client/BotClient.ts
@@ -196,7 +196,7 @@ export default class AshBot extends BaseClient {
 	private initServer() {
 		const PORT = process.env.PORT ?? 8080;
 		const app = express();
-		app.use('/', express.json({ limit: '500' }) as RequestHandler);
+		app.use(express.json({ limit: '500' }) as RequestHandler);
 		app.use(cors());
 		app.use('/', hookRouter);
 
