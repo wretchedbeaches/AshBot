ada5baac31d8fe11999f5a0acd7d87c50c42f7e6
psibean <psionatix@gmail.com>
2021-10-15 22:04:47 +1030

    Initial update of NestearchCOmmand

--- src/commands/Search/NestsearchCommand.ts
+++ src/commands/Search/NestsearchCommand.ts
@@ -1,83 +1,90 @@
-import { Command } from 'discord-akairo';
-import { Message, TextChannel } from 'discord.js';
+import { CommandInteraction, MessageEmbed } from 'discord.js';
 import ntim from '../../util/name_to_id_map.json';
 import { nests } from '../../manualdbModels/nests';
 import { parseNestDb } from '../../util/parse';
-import { FieldsEmbed } from 'discord-paginationembed';
 import util from '../../util/util.json';
 import masterfile from '../../util/masterfile.json';
+import Command from '../../struct/commands/Command';
+import { PokemonTypeDataType } from '../../models/Data';
+import { ButtonPaginator } from '@psibean/discord.js-pagination';
 
-export default class NestsearchCommand extends Command {
-  public constructor() {
-    super('nestsearch', {
-      aliases: ['nestsearch'],
-      category: 'Search',
-      description: {
-        content: 'Search for nests by pokemon name.',
-        usage: 'nestsearch `pokemon name`',
-        examples: ['nestsearch pikachu'],
-      },
-      ratelimit: 3,
-      args: [
-        {
-          id: 'name',
-          type: (_: Message, str: string): null | string => {
-            if (Object.keys(ntim).includes(str.toLowerCase()))
-              return str.toLowerCase();
-            return null;
-          },
-          prompt: {
-            optional: false,
-            start: (msg: Message) =>
-              `${msg.author}, please provide a valid pokemon name.`,
-            retry: (msg: Message) =>
-              `${msg.author}, please provide a valid pokemon name.`,
-          },
-          match: 'rest',
-        },
-      ],
-    });
-  }
+export default class NestSearchCommand extends Command {
+	public constructor() {
+		super('nestsearch', {
+			description: {
+				content: 'Search for nests by pokemon name.',
+				usage: 'nestsearch `pokemon name`',
+				examples: ['nestsearch pikachu'],
+			},
+			category: 'Search',
+			rateLimit: 3,
+		});
+		this.data.addStringOption((nameOption) =>
+			nameOption.setName('name').setDescription('The name of the pokemon to search by'),
+		);
+	}
 
-  public async exec(message: Message, args) {
-    const pokemon_id = ntim[args.name.toLowerCase()];
-    const dbNests = await nests.findAll({
-      where: {
-        pokemon_id: pokemon_id,
-      },
-    });
-    // sending confirmation message
-    const confirmation = `Found ${dbNests.length} ${args.name} Nests`;
-    message.channel.send(confirmation);
-    if (dbNests.length > 0) {
-      // creating and sending paginated embed with results
-      let fields = dbNests.map((pokemon) => parseNestDb(pokemon));
-      const embed = new FieldsEmbed();
-      embed.embed = this.client
-        .embed(message.guild.id)
-        .setTitle(
-          `${args.name.charAt(0).toUpperCase()}${args.name.substring(1)} ` +
-            (this.client.nestMigrationDate
-              ? `Nests (Next Migration: ${this.client.nestMigrationDate.toLocaleDateString()})`
-              : '')
-        )
-        .setColor(
-          `#${util.types[
-            masterfile.pokemon['' + pokemon_id].types[0]
-          ].color.toString(16)}`
-        )
-        .setThumbnail(
-          `https://play.pokemonshowdown.com/sprites/xyani/${args.name.toLowerCase()}.gif`
-        );
-      embed
-        .setArray(fields.length > 0 ? fields : [{ value: 'No nests found.' }])
-        .setAuthorizedUsers([message.author.id])
-        .setChannel(message.channel as TextChannel)
-        .setPageIndicator(true)
-        .setElementsPerPage(parseInt(process.env.FIELDS_LENGTH))
-        .formatField('Nests', (el) => (el as any).value)
-        .setTimeout(300000)
-        .build();
-    }
-  }
+	public async execute(interaction: CommandInteraction) {
+		const nameArgument = interaction.options.getString('name', true);
+		const pokemonId = ntim[nameArgument.toLowerCase()];
+		if (!pokemonId) return interaction.editReply(`The name '${nameArgument}' could not be resolved to a Pokemon`);
+		const dbNests = await nests.findAll({
+			where: {
+				pokemon_id: pokemonId,
+			},
+		});
+
+		if (dbNests.length > 0) {
+			// creating and sending paginated embed with results
+			const nestCount = dbNests.length;
+			const fieldLength = parseInt(process.env.FIELDS_LENGTH ?? '6', 10);
+			const maxPages = nestCount / fieldLength + (nestCount % fieldLength > 0 ? 1 : 0);
+			const pages: MessageEmbed[] = [];
+			const messageOptionsResolver = ({
+				newIdentifiers,
+				paginator,
+			}: {
+				newIdentifiers;
+				paginator: ButtonPaginator;
+			}) => {
+				return {
+					content: `Page ${(newIdentifiers.pageIdentifier as number) + 1}/${paginator.maxNumberOfPages as number}`,
+				};
+			};
+			for (let i = 0; i < maxPages; i++) {
+				const embed = this.client.embed(interaction.guildId);
+				const sliceStart = i * fieldLength;
+				const sliceEnd = sliceStart + fieldLength > nestCount ? nestCount : sliceStart + fieldLength;
+				embed.setTitle(
+					`${nameArgument.charAt(0).toUpperCase()}${nameArgument.substring(1)} ${
+						this.client.nestMigrationDate
+							? `Nests (Next Migration: ${this.client.nestMigrationDate.toLocaleDateString()})`
+							: ''
+					}`,
+				);
+				embed.setThumbnail(`https://play.pokemonshowdown.com/sprites/xyani/${nameArgument.toLowerCase()}.gif`);
+				embed.setColor(
+					`#${
+						util.types[
+							Object.values(masterfile.pokemon[pokemonId].types as PokemonTypeDataType)[0].typeName
+						].color.toString(16) as string
+					}`,
+				);
+				embed.setDescription(
+					`**Nests**\n${dbNests
+						.slice(sliceStart, sliceEnd)
+						.map((pokestopValue) => parseNestDb(pokestopValue))
+						.join()
+						.trim()}`,
+				);
+				pages.push(embed);
+			}
+			const buttonPaginator = new ButtonPaginator(interaction, {
+				pages,
+				maxNumberOfPages: maxPages,
+				messageOptionsResolver,
+			});
+			return buttonPaginator.send();
+		}
+	}
 }
