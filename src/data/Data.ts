3b864a7e222d5ca5c5536763c5442562f5af2eb1
psibean <psionatix@gmail.com>
2021-10-23 12:56:17 +1030

    Add central data export

---
+++ src/data/Data.ts
@@ -0,0 +1,27 @@
+import gruntTypesData from './grunt-types.json';
+import itemsData from './items.json';
+import pokemonMovesData from './pokemon-moves.json';
+import pokemonTypesData from './pokemon-types.json';
+import pokemonFormsData from './pokemon-forms.json';
+import pokemonData from './pokemon.json';
+import questCondtionsData from './quest-conditions.json';
+import questRewardTypesData from './quest-reward-types.json';
+import questTypesData from './quest-types.json';
+import teamsData from './teams.json';
+import throwTypesData from './throw-types.json';
+import weatherData from './weather.json';
+
+export {
+	gruntTypesData,
+	itemsData,
+	pokemonFormsData,
+	pokemonMovesData,
+	pokemonTypesData,
+	pokemonData,
+	questCondtionsData,
+	questRewardTypesData,
+	questTypesData,
+	teamsData,
+	throwTypesData,
+	weatherData,
+};
